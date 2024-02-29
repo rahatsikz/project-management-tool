7bb43838ba633365416729050e267febd0bcbea1
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-02-29 18:03:38 +0600

    [done] ~ readme added

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,3 +1,3 @@
 export const getBaseUrl = (): string => {
-  return 'https://taskhacks-backend.vercel.app/api/v1' || "http://localhost:3030/api/v1";
+  return 'https://taskhacks-backend-ecru.vercel.app/api/v1' || "http://localhost:3030/api/v1";
 };
